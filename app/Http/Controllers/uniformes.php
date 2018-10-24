df62a127f3050b4db598cbdd026d51b372a2be56
Lucas <al221611674@gmail.com>
2018-10-24 12:30:07 -0500

    Update app/Http/Controllers/uniformes.php

--- app/Http/Controllers/uniformes.php
+++ app/Http/Controllers/uniformes.php
@@ -6,10 +6,5 @@ use Illuminate\Http\Request;
 
 class uniformes extends Controller
 {
-    public function areacirculo($radio)
-    {
-        $pi = 3.14;
-        $area = $pi * $radio * $radio;
-        echo "el area circulo es: ". $area;
-    }
+ 
 }
