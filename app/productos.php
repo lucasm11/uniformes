a6c1e308ce8b7ef74f7fba8541101a6ef2a0fc25
HugoSz98 <44390667+HugoSz98@users.noreply.github.com>
2018-10-26 00:57:21 -0500

    productos
    productos modificado

--- app/productos.php
+++ app/productos.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class productos extends Model
 {
-    //
+    protected $primaryKey = 'id_prod';  
+   protected $fillable=['id_prod','escuela','tipo','talla',
+                       'disponoble','ubicacion','precio',id_escuela','id_det','id_orden'];
 }
